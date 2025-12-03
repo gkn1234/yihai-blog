891454fc4b7e4ab7abec250badb23bdb9d36fe98
MacBook Air <macbookair@MacBookdeMacBook-Air-2.local>
2025-12-03 11:54:03 +0800

    first commit

---
+++ content/index.md
@@ -0,0 +1,13 @@
+# heading1
+
+Here is some content.
+
+```ts
+const a = 1
+```
+
+## heading2
+
+### heading3
+
+#### heading4
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', 'nuxt-studio'],
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3, // include h3 headings
+        },
+      },
+    },
+  },
+})
